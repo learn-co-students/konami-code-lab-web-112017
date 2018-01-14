2b7a8e6ffd41a477525fad609258920f9a30ef48
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-01-14 11:41:53 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let i = 0
+  document.addEventListener('keydown', function(e) {
+    if (code[i] === parseInt(e.which || e.detail)) {
+      console.log(`good job! you've gotten ${i+1} out of ${code.length} correct!`)
+      i++
+      if (i === code.length) {
 
+        window.alert("Hurray!")
+        i = 0
+      }
+    } else {
+      i = 0
+    }
+  })
 }
